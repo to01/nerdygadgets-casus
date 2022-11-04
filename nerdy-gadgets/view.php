4a783530bf156b363f5abd5ef3658070efb06e1b
Suppergeert123 <117090884+Suppergeert123@users.noreply.github.com>
2022-11-04 11:08:51 +0100

    Update view.php

--- nerdy-gadgets/view.php
+++ nerdy-gadgets/view.php
@@ -5,6 +5,43 @@ include __DIR__ . "/header.php";
 $StockItem = getStockItem($_GET['id'], $databaseConnection);
 $StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
 ?>
+<?php
+include "cartfuncties.php";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Artikelpagina (geef ?id=.. mee)</title>
+</head>
+<body>
+
+<?php
+//?id=1 handmatig meegeven via de URL (gebeurt normaal gesproken als je via overzicht op artikelpagina terechtkomt)
+if (isset($_GET["id"])) {
+    $stockItemID = $_GET["id"];
+} else {
+    $stockItemID = 0;
+}
+?>
+<h1>Product <?php print($stockItemID) ?></h1>
+
+<!-- formulier via POST en niet GET om te zorgen dat refresh van pagina niet het artikel onbedoeld toevoegt-->
+<form method="post">
+    <input type="number" name="stockItemID" value="<?php print($stockItemID) ?>" hidden>
+    <input type="submit" name="submit" value="Voeg toe aan winkelmandje" Style = "Height: 30px; width:250px; background-color:#23232F; color:#676EFF; border-color: #676EFF">
+</form>
+
+<?php
+if (isset($_POST["submit"])) {              // zelfafhandelend formulier
+    $stockItemID = $_POST["stockItemID"];
+    addProductToCart($stockItemID);         // maak gebruik van geïmporteerde functie uit cartfuncties.php
+    print("Product toegevoegd aan <a href='cart.php'> winkelmandje!</a>");
+}
+?>
+
+</body>
+</html>
 <div id="CenteredContent">
     <?php
     if ($StockItem != null) {
