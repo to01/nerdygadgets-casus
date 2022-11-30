1c5e610c1d7bce8571b77c67ca6daeac3d0575dd
Suppergeert123 <117090884+Suppergeert123@users.noreply.github.com>
2022-11-30 10:59:43 +0100

    Update InlogPagina.php

--- nerdy-gadgets/InlogPagina.php
+++ nerdy-gadgets/InlogPagina.php
@@ -3,33 +3,24 @@ include "header.php";
 ?>
 <table style="width: 100%">
     <tr>
-        <th colspan="3" style="width: 50%">Inloggen</th>
+        <h1 class="TextMain" style="margin-left: 5px; margin-top: 5px">Inloggen </h1>
     </tr>
+</table>
     <form method="post">
-        <tr>
-            <td style="width: 3%">
-                <label for="LoginEmail">Email:</label>
-            </td>
-            <td>
-                <input style="height: 10%" name="LoginEmail" type="email" required>
-            </td>
-        </tr>
-        <tr>
-            <td style="width: 3%">
-                <label for="LoginWachtwoord">Wachtwoord:</label>
-            </td>
-            <td>
-                <input style="height: 10%" name="LoginWachtwoord" type="password" minlength="8" required>
-            </td>
-            <td style="width: 22%"></td>
-            <td style="width: 3%">
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input style="height: 10%" type="submit" value="Inloggen">
-            <td></td>
-        </tr>
+        <table>
+            <tr> <!--e-mail -->
+                <td><div style="text-align: right">* E-mail:</div></td>
+                <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="E-mail" type="email" required></td>
+            </tr>
+            <tr> <!--Wachtwoord -->
+                <td><div style="text-align: right">* Wachtwoord:</div></td>
+                <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="wachtwoord" type="password" required></td>
+            </tr>
+            <tr>
+                <td style="width: 160px"></td>
+                <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" type="submit" name="BestelSubmit" value="Inloggen" ></td>
+            </tr>
+        </table>
     </form>
 </table>
 <a href="RegisteerPagina.php" style="color:#3366CC;" class="HrefDecoration"> <u> Geen account? Klik hier </u> </a>
