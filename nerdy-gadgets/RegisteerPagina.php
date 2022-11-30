5c168abbd133b4d8ffbd2c6aa21e1a3bdbdb4614
Suppergeert123 <117090884+Suppergeert123@users.noreply.github.com>
2022-11-30 11:00:16 +0100

    Update RegisteerPagina.php

--- nerdy-gadgets/RegisteerPagina.php
+++ nerdy-gadgets/RegisteerPagina.php
@@ -3,32 +3,44 @@ include "header.php";
 ?>
 <table style="width: 100%">
     <tr>
-        <th colspan="3" style="width: 50%">Registreren</th>
+        <h1 class="TextMain" style="margin-left: 5px; margin-top: 5px">Registreren </h1>
     </tr>
-    <form method="post">
-        <tr>
-            <td style="width: 3%">
-                <label for="RegisterEmail">Email:</label>
-            </td>
-            <td>
-                <input style="height: 10%" name="RegisterEmail" type="email" required>
-            </td>
+</table>
+<form method="post">
+    <table>
+        <tr> <!--naam -->
+            <td><div style="text-align: right">* Naam:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="BestelNaam" type="text" required></td>
+        </tr>
+        <tr> <!--Wachtwoord -->
+            <td><div style="text-align: right">* Wachtwoord:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="wachtwoord" type="password" required></td>
+        </tr>
+        <tr> <!--e-mail -->
+            <td><div style="text-align: right">* E-mail:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="E-mail" type="email" required></td>
+        </tr>
+        <tr> <!--Land -->
+            <td><div style="text-align: right">* Land:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="Land" type="country" required></td>
+        </tr>
+        <tr> <!-- adres -->
+            <td><div style="text-align: right">* Adres:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="BestelAdres" type="text" required></td>
+        </tr>
+        <tr> <!-- postcode -->
+            <td><div style="text-align: right">* Postcode:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" name="BestelPostcode" type="text" required></td>
+        </tr>
+        <tr> <!-- telefoonnummer -->
+            <td><div style="text-align: right">* Telefoonnummer:</div></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" type="tel" id="phone" name="phone" minlength="10" maxlength="15" pattern="[0,9]{3}[0-9]{8,15}\[0,9]{3} [0-9]{8,15}" required></td>
         </tr>
         <tr>
-            <td style="width: 3%">
-                <label for="RegisterWachtwoord">Wachtwoord:</label>
-            </td>
-            <td>
-                <input style="height: 10%" name="RegisterWachtwoord" type="password" minlength="8" required>
-            </td>
-            <td style="width: 22%"></td>
-            <td style="width: 3%">
+            <td style="width: 160px"></td>
+            <td><input style="height: 10%; background-color:#23232F; color:#676EFF; border-color:#676EFF" type="submit" name="Inlogsubmit" value="Registreren" onclick="return confirm('Zijn alle gegevens goed ingevuld en gecontroleerd?')"></td>
         </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input style="height: 10%" type="submit" value="Registreren">
-            <td></td>
-        </tr>
-    </form>
+    </table>
+</form>
+</table>
 </table>
