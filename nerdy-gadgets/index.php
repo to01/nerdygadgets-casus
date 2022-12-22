e928151575bb416f9b43b6140ebd4177dd76335a
Suppergeert123 <117090884+Suppergeert123@users.noreply.github.com>
2022-12-22 11:08:45 +0100

    Add files via upload

--- nerdy-gadgets/index.php
+++ nerdy-gadgets/index.php
@@ -1,23 +1,103 @@
 <!-- dit is het bestand dat wordt geladen zodra je naar de website gaat -->
 <?php
 include __DIR__ . "/header.php";
+
+$host = "localhost";
+$user = "root";
+$pass = ""; //eigen password invullen
+$databasename = "nerdygadgets";
+$port = 3306;
+$connection = mysqli_connect($host, $user, $pass, $databasename, $port);
+
+$aanbevolen = array();
+
+$query = "SELECT StockItemID, Korting FROM aanbevolen";
+$result = mysqli_query($connection, $query);
+while($row = mysqli_fetch_array($result)) {
+    $aanbevolen[$row["StockItemID"]] = $row["Korting"];
+}
+
 ?>
-<div class="IndexStyle">
-    <div class="col-11">
-        <div class="TextPrice">
-            <a href="view.php?id=93">
-                <div class="TextMain">
-                    "The Gu" red shirt XML tag t-shirt (Black) M
-                </div>
-                <ul id="ul-class-price">
-                    <li class="HomePagePrice">€30.95</li>
-                </ul>
-        </div>
-        </a>
-        <div class="HomePageStockItemPicture"></div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang="nl" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="UTF-8">
+    <title>Aanbiedingen</title>
+</head>
+<body>
+<center>
+    <?php
+    if(empty($aanbevolen)){
+        print("<br<br><br><br><br<br><br><br><br<br><br><br><br<br><br><br><br<br><br<br><h1 class='Aanbiedingen' style='color: #676EFF'>Er zijn momenteel geen aanbiedingen!</h1>");
+    } else {
+        print("<h1 class='Aanbiedingen' style='color: #676EFF'>Aanbiedingen!</h1>");
+    }
+    ?>
+</center>
+<?php
+
+if(!empty($aanbevolen)) {
+    foreach ($aanbevolen as $id => $korting) {
+
+        if ($id === "") continue;
+
+        $query = "SELECT ImagePath FROM stockitemimages WHERE StockItemID = " . $id;
+        $result = mysqli_query($connection, $query);
+        $row = mysqli_fetch_row($result);
+
+        if (empty($row[0])) {
+            $query = "SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = " . $id;
+            $result = mysqli_query($connection, $query);
+            $row = mysqli_fetch_row($result);
+            $path = "Public/StockGroupIMG/$row[0]";
+        } else {
+            $path = "Public/StockItemIMG/" . $row[0];
+        }
+
+        $query = "SELECT StockItemName FROM stockitems WHERE StockItemID = " . $id;
+        $result = mysqli_query($connection, $query);
+        $row = mysqli_fetch_row($result);
+        $name = $row[0];
+
+        $query = "SELECT (RecommendedRetailPrice * (1+(TaxRate/100))) FROM stockitems WHERE StockItemID = " . $id;
+        $result = mysqli_query($connection, $query);
+        $row = mysqli_fetch_row($result);
+        $prijs = $row[0];
+        $korting1 = $prijs * ($korting / 100);
+        $prijs1 = $prijs - $korting1;
+
+        $query = "SELECT QuantityOnHand FROM stockitemholdings WHERE StockItemID = " . $id;
+        $result = mysqli_query($connection, $query);
+        $row = mysqli_fetch_array($result);
+        $voorraad = $row[0];
+        print("
+    <table style='float: left; width: 300px'>
+    <tr>
+    <td colspan='3'><img style='width: 200px;' src='$path'></td>
+    </tr>
+    <tr>
+    <td colspan='3'><a href='view.php?id=" . $id . "' style='color: &#xf1f8'>Artikelnummer: $id</td>
+    </tr>
+    <tr>
+    <td colspan='3'>Naam: $name<td>
+    </td>
+    <tr>
+    <td colspan='3'>Voorraad: $voorraad</td>
+    </tr>
+    <tr>
+    <td colspan='3'>Prijs inclusief BTW: " . sprintf('€ %.2f', $prijs1) . "</td>
+    </tr>
+    <tr>
+    <td colspan='3' style='color: gold'>Korting: $korting%</td>
+    </tr>
+    ");
+    }
+}
+?>
+</table>
+<hr style='background-color: #676EFF'>
+</body>
+</html>
 <?php
 include __DIR__ . "/footer.php";
 ?>
-
--- nerdy-gadgets/view.php
+++ nerdy-gadgets/view.php
@@ -1,6 +1,10 @@
 <!-- dit bestand bevat alle code voor de pagina die één product laat zien -->
 <?php
 include __DIR__ . "/header.php";
+include_once "database.php";
+$databaseConnection = connectToDatabase();
+$StockItem = getStockItem($_GET['id'], $databaseConnection);
+$StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
 $host = "localhost";
 $user = "root";
 $pass = ""; //eigen password invullen
@@ -8,8 +12,6 @@ $databasename = "nerdygadgets";
 $port = 3306;
 $connection = mysqli_connect($host, $user, $pass, $databasename, $port);
 
-$StockItem = getStockItem($_GET['id'], $databaseConnection);
-$StockItemImage = getStockItemImage($_GET['id'], $databaseConnection);
 ?>
 
 <?php
@@ -112,28 +114,58 @@ if($r){
             <h2 class="StockItemNameViewSize StockItemName">
                 <?php print $StockItem['StockItemName']; ?>
             </h2>
-            <div class="QuantityText"><?php print $StockItem['QuantityOnHand']; ?></div>
-            <input style="background-color:#23232F; color: white; border-color:#676EFF; width: 50px; height: 30px; margin-top: 130px" type="button" value="Live" onclick="window.location.reload()">
             <div id="refresh_me" >
                 <?php
                 if (isset($_GET["id"])) {
-                $stockItemID = $_GET["id"];
-                    if($stockItemID>= 220){
+                    $stockItemID = $_GET["id"];
+                    if($stockItemID>= 220 && $stockItemID <=227){
+                        $query = "SELECT StockItemID FROM stockitems WHERE IsChillerStock = 1 ";
+                        $result = mysqli_query($connection, $query);
+                        $row = mysqli_fetch_array($result);
+                        if(isset($row[0])){
+                        ?> <input style="background-color:#23232F; color: white; border-color:#676EFF; width: 50px; height: 30px; margin-top: 130px" type="button" value="Live" onclick="window.location.reload()"><br> <?php
                         $query = "SELECT Temperature, ValidFrom FROM coldroomtemperatures WHERE ColdRoomSensorNumber = 5";
                         $result = mysqli_query($connection, $query);
                         $row = mysqli_fetch_array($result);
                         print ("Temperatuur Magazijn: ") . $row["Temperature"]."˚C <br>";
                         print ("Tijd van meting: ") . $row["ValidFrom"];
-                }}?>
+                    }}}?>
             </div>
+            <div class="QuantityText"><?php print $StockItem['QuantityOnHand']; ?></div>
             <div id="StockItemHeaderLeft">
                 <div class="CenterPriceLeft">
                     <div class="CenterPriceLeftChild">
                         <br>
                         <br>
-                        <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $StockItem['SellPrice']); ?></b></p>
-                        <h6 class="nobreak"> Inclusief BTW </h6><br><br>
+                        <?php
+                        $query = "SELECT StockItemID FROM Aanbevolen WHERE StockItemID = " . $StockItem["StockItemID"];
+                        $result = mysqli_query($connection, $query);
+                        $row = mysqli_fetch_array($result);
+                        if(empty($row)){
+                            ?>
+                            <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $StockItem["SellPrice"]); ?></b></p>
+                            <h6 class="nobreak"> Inclusief BTW </h6><br><br>
+                            <?php
+                        } else {
+                            $aanbevolen = $row[0];
 
+                            if(empty($aanbevolen)){
+                                } else {
+                                $query = "SELECT Korting FROM Aanbevolen WHERE StockItemID = " . $StockItem["StockItemID"];
+                                $result = mysqli_query($connection, $query);
+                                $row = mysqli_fetch_array($result);
+                                $korting = $row[0];
+                                $prijs = $StockItem["SellPrice"];
+                                $korting1 = $prijs * ($korting / 100);
+                                $prijs1 = $prijs - $korting1;
+                            ?>
+                            <p class="StockItemPriceText"><b><?php print sprintf("€ %.2f", $prijs1); ?></b></p>
+                            <h6 class="nobreak"> Inclusief BTW </h6><br><br>
+                                <p class="nobreak" style="color: gold; font-size: xx-large">Aanbieding!</p>
+                            <?php
+                            }
+                        }
+                        ?>
                         <?php
                         //?id=1 handmatig meegeven via de URL (gebeurt normaal gesproken als je via overzicht op artikelpagina terechtkomt)
                         if (isset($_GET["id"])) {
@@ -147,17 +179,16 @@ if($r){
                         <form method="post">
                             <input type="number" name="stockItemID" value="<?php print($stockItemID) ?>" hidden>
                             <input type="submit" name="submit" value="Voeg toe aan winkelmandje" style="height:30px; width:250px; background-color:#23232F; color:#676EFF; border-color:#676EFF">
-                            <input type="number" name="amount" value="1" style="height:30px; width:250px; background-color:#23232F; color:#FFFFFF; border-color:#676EFF">
+                            <input type="number" name="amount" value="1" min="1" style="height:30px; width:250px; background-color:#23232F; color:#FFFFFF; border-color:#676EFF">
                         </form>
                         <?php
-
                         if (isset($_POST["submit"])) {              // zelfafhandelend formulier
                             $query = "SELECT QuantityOnHand FROM stockitemholdings WHERE StockItemID = " . $stockItemID;
                             $result = mysqli_query($connection, $query);
                             $row = mysqli_fetch_array($result);
                             $hoeveelheid = $row[0];
                             $cart = getCart();
-                            if(empty($cart["$stockItemID"])) {
+                            if(empty($cart["$stockItemID"])){
                                 $cart["$stockItemID"] = 0;
                             }
                             if ($_POST["amount"] > $hoeveelheid || ($_POST["amount"] + $cart["$stockItemID"]) > $hoeveelheid) {
@@ -166,7 +197,7 @@ if($r){
                                 $stockItemID = $_POST["stockItemID"];
                                 if ($_POST["amount"] >= 1 && $_POST["amount"] == number_format($_POST["amount"], 0, ',', '.')) {
                                     $_POST["amount"] = rtrim(rtrim(number_format($_POST["amount"], '0')));
-                                    addProductAmountToCart($stockItemID, $_POST["amount"]);// maak gebruik van geïmporteerde functie uit cartfuncties.php
+                                    addProductAmountToCart($stockItemID, $_POST["amount"]);         // maak gebruik van geïmporteerde functie uit cartfuncties.php
                                     print("Product toegevoegd");
                                     print("<meta http-equiv='refresh'  content='0'>");
                                 } elseif ($_POST["amount"] >= 1) {
@@ -177,10 +208,7 @@ if($r){
                                     print("Voer een geldig cijfer in!");
                                 }
                             }
-
-                        }
-                        print("<a href='cart.php'> Winkelmand bekijken</a>");
-
+                        }print("<a href='cart.php'> Winkelmand bekijken</a>");
                         ?>
                     </div>
                 </div>
@@ -198,8 +226,8 @@ if($r){
             if (is_array($CustomFields)) { ?>
                 <table>
                 <thead>
-                <th>Naam:</th>
-                <th>Data: </th>
+                <th>Naam</th>
+                <th>Data</th>
                 </thead>
                 <?php
                 foreach ($CustomFields as $SpecName => $SpecText) { ?>
